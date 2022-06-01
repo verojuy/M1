a6a650f93e423b0f83b0b2c47916f2fb67e54c95
verojuy <verojuy.gonzalez@gmail.com>
2022-05-31 22:08:43 -0300

    07

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,14 +6,37 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-
-}
+  var div = 2;
+  var array = [1];
+  while(num !==1)
+  if(num % div === 0){
+    array.push(div);
+    num= num/ div;
+  }else{
+    div++
+  }
+  return array;
+  
+ }
 
 function bubbleSort(array) {
   // Implementar el método conocido como bubbleSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  for( let i = 0 ; i < array.length-1;i++){                   //
+    for(let j = 0 ; j < array.length-1 ; j++){
+      if(array[j]> array[j+1]){
+        let aux =array[j];                                    //para guardar el valor de j y no perderlo
+        array[j]= array [j+1];                                 // para intercambiar los lugares
+        array[j+1]= aux;
+      }
+    }
+  
+    
+  }
+  return array;
+  
 
 }
 
@@ -23,6 +46,18 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+  for(var i= 1 ; i< array.length; i ++){
+    var j = i -1
+     var aux=array[i];              //aca se va guardar el valor auxiliar
+      while(j>=0 && array[j]> aux){
+        array[j+1] = array[j]/// mueve el valor a la derecha
+        j--;
+      } 
+      array[j+1] = aux;
+    }
+    return array;
+
+
 
 }
 
@@ -32,6 +67,20 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+  for( var i = 0; i < array.length-1; i ++){
+    let posmin = i;
+    for( var j = i +1; j < array.length;j++){
+      if(array[j] <array[posmin]){              // si el array en su posiscion j es menor que el array en su posicion posmin entonces que j se desplaze hast j
+        posmin = j;                             // se desplaza j hasta donde encuentre un numero en que no se cumpla con la condicion
+      }
+    }
+    if( i !== posmin){
+      var aux = array[i];                    // guardo el valor de i 
+    array[i]= array[posmin];                //invierte el lugar de los numeros  lo acomoda
+    array[posmin] = aux;
+    }
+  }
+  return array;
 
 }
 
